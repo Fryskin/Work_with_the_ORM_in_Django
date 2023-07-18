f9befd9165680981fc08f6553f7bd90d33abe2a5
Fryskin <gman358325@gmail.com>
2023-07-18 17:58:52 +0200

    Blog

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index, home
+from main.views import home, ProductListView, BlogCreateView, BlogListView, BlogDetailView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('home/', home, name='home')
+    path('', ProductListView.as_view(), name='index'),
+    path('home/', home, name='home'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('', BlogListView.as_view(), name='list'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
+    # path('edit/<int:pk>/', ..., name='edit'),
+    # path('delete/<int:pk>/', ..., name='delete'),
+    # path('')
 ]
--- main/views.py
+++ main/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
-from main.models import Product
+from main.models import Product, Blog
 
 
-def index(request):
-    products_list = Product.objects.all()
-    content = {'object_list': products_list,
-               'title': 'Main'}
+class ProductListView(ListView):
+    model = Product
+    template_name = 'main/index.html'
 
-    return render(request, 'main/index.html', content)
 
+# class HomeListView(ListView):
+#     model = Product
+#     template_name = 'main/home.html'
 
 def home(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+        email = request.POST.get('email')                           # Эту функцию не перевести в cbv, так как она
+        message = request.POST.get('message')                       # отвечает за статичную информацию.
         print(f'{name} ({email}): {message}')
 
     content = {'title': 'Contacts'}
 
     return render(request, 'main/home.html', content)
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'slug', 'body', 'preview', 'date_of_creation', 'count_of_views',)
+    success_url = reverse_lazy('main:index')
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
